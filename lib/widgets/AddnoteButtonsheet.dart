126f2606908be987d0a9bdf723e999ac03339e2a
Abdelrhman Ahmed <aa8273@fayoum.edu.eg>
2026-05-13 18:09:26 +0200

    finsh notes botton sheet

--- lib/widgets/AddnoteButtonsheet.dart
+++ lib/widgets/AddnoteButtonsheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/widgets/custom_buttom.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddnoteButtonsheet extends StatelessWidget {
@@ -8,13 +10,17 @@ class AddnoteButtonsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 20),
-          CustomTextField(hint: 'Title'),
-          SizedBox(height: 20),
-          CustomTextField(hint: 'Content', maxlines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            CustomTextField(hint: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'Content', maxlines: 5),
+            SizedBox(height: 24),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_buttom.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: KprimaryColor,
+
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: GestureDetector(
+        onTap: () {},
+        child: Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
